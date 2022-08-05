08b55e65a7e29bfa17c1b4c802ae17aafd0a03b8
M Zaenal <zendzzo@gmail.com>
2022-08-05 09:41:31 +0700

    fix typo on customer add

--- resources/views/livewire/customer-create.blade.php
+++ resources/views/livewire/customer-create.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <form class="horizontal-form" wire:submit.prevent="addSupplier">
+    <form class="horizontal-form" wire:submit.prevent="addCustomer">
         <div class="form-group row">
             <div class="col-12 col-md-3 text-right">
                 <label for="name">Nama</label>
